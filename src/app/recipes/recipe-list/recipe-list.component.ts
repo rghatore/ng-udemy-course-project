ba8c24055095acea002a92335c9473fab3075455
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-03-02 11:03:01 -0700

    setting up recipes components structures

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Cashew Magic', `Add some magin to your cashew!`, 'https://images.unsplash.com/photo-1589706705875-774562f60260?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
+  ];
 
   constructor() { }
 
